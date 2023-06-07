b3d19dab34145f6cace344c74e9591bf6c9aab5b
manuelVerdugoK <m.verdugok@duocuc.cl>
2023-06-07 13:22:46 -0400

    cambios

--- miPlan/settings.py
+++ miPlan/settings.py
@@ -127,11 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-#STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_URL = 'static/'
-STATIC_ROOT  =  "/home/verko/ChauchaApp/static/"
+#STATIC_URL = 'static/'
+#STATIC_ROOT  =  "/home/verko/ChauchaApp/static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
